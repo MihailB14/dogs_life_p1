cca0c8380bec02b2ba878fd6774fb60a9e52f9b7
Mihail <mihaibarbulescu2010yahoo.com>
2023-07-31 10:30:18 +0100

    Commit from 31.07

--- java-api/src/main/java/com/db/grad/javaapi/model/Dog.java
+++ java-api/src/main/java/com/db/grad/javaapi/model/Dog.java
@@ -1,9 +1,23 @@
 package com.db.grad.javaapi.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "dogs")
 public class Dog
 {
+    @Column(name = "dog_id")
+    @Id
     private long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "age")
+    private int age;
 
     public long getId() {
         return id;
@@ -26,5 +40,13 @@ public class Dog
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
--- java-api/src/main/java/com/db/grad/javaapi/repository/DogsRepositoryStub.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/DogsRepositoryStub.java
@@ -1,9 +1,16 @@
 package com.db.grad.javaapi.repository;
 
 import com.db.grad.javaapi.model.Dog;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 public class DogsRepositoryStub implements DogsRepository {
     private ArrayList<Dog> itsDogs = new ArrayList<>();
@@ -70,9 +77,24 @@ public class DogsRepositoryStub implements DogsRepository {
     }
 
     @Override
+    public void deleteAllById(Iterable<? extends Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Dog> entities) {
+
+    }
+
+    @Override
     public  long count()
     {
         return this.itsDogs.size();
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
     }
 
     @Override
@@ -95,4 +117,118 @@ public class DogsRepositoryStub implements DogsRepository {
         itsDogs.clear();
     }
 
+    @Override
+    public List<Dog> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Dog> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<Dog> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<Dog> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    @Override
+    public <S extends Dog> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<Dog> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public <S extends Dog> S saveAndFlush(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Dog> List<S> saveAllAndFlush(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public void deleteAllInBatch(Iterable<Dog> entities) {
+
+    }
+
+    @Override
+    public void deleteAllByIdInBatch(Iterable<Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    @Override
+    public Dog getOne(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public Dog getById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public Dog getReferenceById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public <S extends Dog> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends Dog> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends Dog> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends Dog> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends Dog> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends Dog> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    @Override
+    public <S extends Dog, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
+    }
 }
